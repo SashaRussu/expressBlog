b782d590c8a1048d3dcc27a027a18acfc8309ae2
Russu Sasha <russuSasha19@gmail.com>
2017-08-17 18:31:56 +0300

    '06-wysiwyg-tinymce'

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 /** Сесії */
 app.use(bodyParser.urlencoded({ extended: false }));

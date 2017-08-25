30952cd5e7970839bc3dc6767e94a7b3fd3357d1
Oleg Kalyta <2xyligan@gmail.com>
2017-08-25 15:08:45 +0300

    refactoring 2

--- DAL/category.js
+++ DAL/category.js
@@ -1,24 +1,12 @@
 const Categories = require('./../db/categories');
 
-const get = () => Categories.find()
-
-exports.get = get
-
+exports.get = () => Categories.find()
+exports.edit = (id, name) => Categories.update({ _id: id }, { $set: { name: name } })
+exports.deleteCategory = id => Categories.remove({ _id: id })
 
 const add = name => {
   const category = new Categories({ name })
-
   return category.save()
 }
 
 exports.add = add
-
-
-const edit = (id, name) => Categories.update({ _id: id }, { $set: { name: name } })
-
-exports.edit = edit
-
-
-const deleteCategory = id => Categories.remove({ _id: id })
-
-exports.deleteCategory = deleteCategory
